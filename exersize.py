4aa724fb07260dcbf2f4a9920fd7321d91238908
Anush <anushka.23@ichat.sp.edu.sg>
2024-04-23 15:23:37 +0800

    Updating the lab exersizes

---
+++ exersize.py
@@ -0,0 +1,42 @@
+""" EXERSIZE 1
+
+
+def calculate_bmi(height,weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+calculate_bmi(weight=57, height=1.73)
+
+
+
+
+#weight=float(input("your weight: "))
+#height=float(input("your height: "))
+
+
+
+weight=70
+height=1.73
+bmi= weight/(height*height)
+print("Your BMI is ", bmi)
+if(bmi<18.5):
+    print("Underweight")
+elif((bmi<=25.0) and (bmi>=18.5)):
+    print("Normal Weight")
+else:
+    print("overweight")"""
+
+
+
+
+
+
+def display_main_menu():
+    print("display_main_menu")
+    display_main_menu = 1234567
+
+
+def calc_average(): 
+    print("calc_average") 
+    print( display_main_menu())
+    x= display_main_menu.split()
+    print(x)
