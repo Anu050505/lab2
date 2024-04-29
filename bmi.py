4019158a8fbdff8acfee575584d879e37f650bf5
Anush <anushka.23@ichat.sp.edu.sg>
2024-04-29 15:49:04 +0800

    Y

--- bmi.py
+++ bmi.py
@@ -1,21 +1,21 @@
 
 def calculate_bmi(height,weight):
+    height=input('enter:')
+    weight=input('enter:')
     print("Height = " + str(height))
     print("Weight = " + str(weight))
-calculate_bmi(weight=57, height=1.73)
-
+#calculate_bmi(weight=57, height=1.73)
 
 #weight=float(input("your weight: "))
 #height=float(input("your height: "))
 
-
-weight=70
-height=1.73
-bmi= weight/(height*height)
-print("Your BMI is ", bmi)
-if(bmi<18.5):
-    print("Underweight")
-elif((bmi<=25.0) and (bmi>=18.5)):
-    print("Normal Weight")
-else:
-    print("overweight")
+#weight=70
+#height=1.73
+    bmi= weight/(height*height)
+    print("Your BMI is ", bmi)
+    if(bmi<18.5):
+        print("-1")
+    elif((bmi<=25.0) and (bmi>=18.5)):
+        print("0")
+    else:
+         print("1")
